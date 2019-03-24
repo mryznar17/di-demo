a1b50f4255b16038b0f7a8c90a3c743bc655f556
Marek Ryznar <mryznar17@gmail.com>
2019-03-24 16:20:53 +0100

    Reading properties from OS environment property

--- src/main/java/mryznar/springframework/didemo/DiDemoApplication.java
+++ src/main/java/mryznar/springframework/didemo/DiDemoApplication.java
@@ -35,7 +35,7 @@ public class DiDemoApplication {
 //		System.out.println(ctx.getBean(SetterInjectionController.class).sayHello());
 //		System.out.println(ctx.getBean(ConstructorInjectionController.class).sayHello());
 		FakeDataSource fakeDataSource = ctx.getBean(FakeDataSource.class);
-		System.out.println(fakeDataSource.getUrl());
+		System.out.println(fakeDataSource.getUser());
 	}
 
 }
--- src/main/java/mryznar/springframework/didemo/config/PropertyConfig.java
+++ src/main/java/mryznar/springframework/didemo/config/PropertyConfig.java
@@ -1,28 +1,35 @@
 package mryznar.springframework.didemo.config;
 
 import mryznar.springframework.didemo.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")
 public class PropertyConfig {
 
-    @Value("${guru.user}")
+    @Value("${guru.username}")
     String user;
     @Value("${guru.password}")
     String password;
     @Value("${guru.dburl}")
     String url;
 
+    /**
+     * Access to OS environment
+     */
+    @Autowired
+    Environment env;
 
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-        fakeDataSource.setUser(user);
+        fakeDataSource.setUser(env.getProperty("USERNAME"));
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
         return fakeDataSource;
